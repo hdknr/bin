aa906f800eb9739d859cd5b72a53be83f93cf087
hdknr <gmail@hdknr.com>
2012-05-16 16:30:01 +0900

    X.509 certificate dump

--- bash/openssl.bash
+++ bash/openssl.bash
@@ -1,3 +1,8 @@
 function X509(){
     echo "q" | openssl s_client -showcerts -connect $1:443 2>/dev/null | openssl x509 -noout -text
 }
+
+function X509DUMP()
+{
+    openssl x509 -noout -text -in $1
+}
