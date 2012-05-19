9c5047ea608c1c529fcf7b42e8bd27985c4d77c9
hdknr <gmail@hdknr.com>
2012-05-20 08:19:38 +0900

    OpenSSL export PEM public key from PEM X.509

--- bash/openssl.bash
+++ bash/openssl.bash
@@ -1,8 +1,19 @@
-function X509(){
+function SSL_X509(){
     echo "q" | openssl s_client -showcerts -connect $1:443 2>/dev/null | openssl x509 -noout -text
 }
 
 function X509DUMP()
 {
     openssl x509 -noout -text -in $1
+}
+
+function X509FINGERPRINT()
+{
+    openssl x509 -md5 -noout -fingerprint -in $1
+    openssl x509 -sha1 -noout -fingerprint -in $1
+}
+
+function X509_PUB_FROM_CERT()
+{
+    openssl x509 -pubkey  -inform PEM  -in  $1
 }
