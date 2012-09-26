c9f7657d2db43803ae8b9b1be98ccdf53edd94c3
hdknr <gmail@hdknr.com>
2012-09-27 07:22:00 +0900

    OpenSSL tools added

--- bash/openssl.bash
+++ bash/openssl.bash
@@ -15,5 +15,24 @@ function X509FINGERPRINT()
 
 function X509_PUB_FROM_CERT()
 {
-    openssl x509 -pubkey  -inform PEM  -in  $1
+    openssl x509 -pubkey  -inform PEM  -in  $1 -noout
 }
+
+
+function PKEY_MODULUS()
+{
+    openssl rsa -in $1  -modulus -noout
+}
+function PKEY_EXPONENT()
+{
+    openssl rsa -in $1  -modulus -noout
+}
+function PUBKEY_FROM_PKEY()
+{
+    openssl rsa -in $1  -out $1.pub -pubout
+}
+
+function PUBKEY_DUMP()
+{
+    openssl  pkey -pubin -in $1 -text -noout
+} 
