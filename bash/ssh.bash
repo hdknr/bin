3ed6bb66e8cd12087bfba8fdf578cb54532a7fd5
hdknr <gmail@hdknr.com>
2013-04-20 16:38:39 +0900

    ssh tunnel connection

---
+++ bash/ssh.bash
@@ -0,0 +1,37 @@
+# HUB_HOST=
+# HUB_KEY=
+# HUB_USER=
+
+export TUNNEL_OFFICE=3333
+export TO_OFFCIE=4444 
+export TUNNEL_HOME=5555
+export TO_HOME=6666
+
+function WaitInOffice()
+{
+ssh -i $HUB_KEY  -N -f -g -R $TUNNEL_OFFICE:localhost:22 $HUB_USER@$HUB_HOST
+}
+function WaitInHome()
+{
+ssh -i $HUB_KEY  -N -f -g -R $TUNNEL_HOME:localhost:22 $HUB_USER@$HUB_HOST
+}
+
+function BindToOffcie()
+{
+ssh -i $HUB_KEY  -N -f -g -L $TO_OFFICE:localhost:$TUNNEL_OFFICE $HUB_USER@$HUB_HOST
+}
+function BindToHome()
+{
+ssh -i $HUB_KEY  -N -f -g -L $TO_HOME:localhost:$TUNNEL_HOME $HUB_USER@$HUB_HOST
+}
+
+function SshOffice()
+{
+ssh -p $TO_OFFICE  localhost 
+}
+
+function SshHome()
+{
+ssh -p $TO_HOME localhost 
+}
+}
