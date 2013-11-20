0f47c9de46fb57630dcfa37669ad29e10ccf1bfa
hdknr <gmail@hdknr.com>
2013-11-20 14:32:25 +0900

    Fablic Ubutnu install packages

--- fabric/libs/ubuntu.py
+++ fabric/libs/ubuntu.py
@@ -6,9 +6,15 @@ def uptime():
 
 
 def deb_packages():
-    with open('out.txt','w') as out:
+    with open('out.log','w') as out:
         out.write(  
             cuisine.run('sudo dpkg --get-selections') )
 
+def install_packages():
+    url = 'https://raw.github.com/hdknr/bin/master/fabric/res/ubuntu.packages.list'
+    command = "curl %s | while read p ;do sudo aptitude install $p -y ;done" % url
+    cuisine.run( command  )
+
 def test():
+
     print "test"
