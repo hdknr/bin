f41ef56634e712c3bb2d483ba5257cfa941a54e8
hdknr <gmail@hdknr.com>
2014-12-04 09:23:13 +0000

    PHP commposer installation

--- env/phpenv.bash
+++ env/phpenv.bash
@@ -1,3 +1,8 @@
 export PATH=$HOME/.rbenv/bin:$HOME/.phpenv/bin:$PATH
 eval "$(rbenv init -)"
 eval "$(phpenv init -)"
+
+function PHP_GET_COMPOSER()
+{
+    curl -s http://getcomposer.org/installer | php
+}
