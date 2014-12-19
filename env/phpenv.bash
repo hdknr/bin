19c876b6a2496f2b2b02c7b8163d4dfdab0b6962
hdknr <gmail@hdknr.com>
2014-12-19 03:12:14 +0000

    phpenv related

--- env/phpenv.bash
+++ env/phpenv.bash
@@ -6,3 +6,14 @@ function PHP_GET_COMPOSER()
 {
     curl -s http://getcomposer.org/installer | php
 }
+
+function PHP_SET_VER()
+{
+    export PHP_VER=$(phpenv version|awk '{print $1}');
+}
+
+function PHP_SET_PATH()
+{
+    PHP_SET_VER;
+    export PHP_PATH=$HOME/.phpenv/versions/$PHP_VER;
+}
