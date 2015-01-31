a0dfb138b7bede9e13ad712b893f1c07b334d151
hdknr <gmail@hdknr.com>
2015-01-31 09:16:34 +0900

    ansible playbook source

--- env/ansible.bash
+++ env/ansible.bash
@@ -1,8 +1,9 @@
 function ANSIBLE_PLAYBOOK_DEBINIT()
 {
+    source ~/bin/const.bash;
     YML=debinit.yml
     if [ ! -f $YML ] ; then
-        wget -O $YML https://gist.githubusercontent.com/hdknr/25b5beaceb896bf11a7b/raw/0efd69be7fb5e85e69f70c32dc37f9a11187efd2/debinit.yml
+        wget -O $YML $BINGIT/env/ansible/debinit.yml
     fi;
     
     echo "## put the following code in Vagrant file.";
