b4086392da1816f76a6dc20aa25cf0532fa4048d
hdknr <gmail@hdknr.com>
2015-02-06 21:57:44 +0000

    django.bash

--- env/django.bash
+++ env/django.bash
@@ -1,8 +1,9 @@
 function DJ()
 {
+    PARAMS="$@"; [ -n "$PARAMS" ] || PARAMS="shell";
     C=$PWD;
     while [ "$C" != / ]; do
-        [ -f "$C/manage.py" ] && { python $C/manage.py $@ ; break; };
+        [ -f "$C/manage.py" ] && { python $C/manage.py $PARAMS ; break; };
         C=`dirname $C`
     done;
 }
