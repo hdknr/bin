3faaf99e21d038d7958b7ea9261c7bdd7fdb53c1
hdknr <gmail@hdknr.com>
2015-04-22 08:28:07 +0000

    anyenv.bash

--- setup/anyenv.bash
+++ setup/anyenv.bash
@@ -1,2 +1,45 @@
 #!/bin/bash
+source `dirname $0`/conf.bash
+BIN_INIT anyenv
+#
+if [ -d ".anyenv" ] ; then
+    BIN_EXIT;
+fi
+#
+case "$BIN_OS" in 
+  "DEBIAN") 
+    PKGS=(
+    build-essential
+    curl
+    libbz2-dev
+    libreadline-dev
+    libsqlite3-dev
+    libssl-dev
+    llvm
+    make
+    wget
+    zlib1g-dev
+    );;
+ "CENTOS")   
+    PKGS=(
+    bzip2
+    bzip2-devel
+    gcc
+    gcc-c++
+    git
+    make
+    openssl-devel
+    patch
+    python26 
+    python26-devel
+    readline-devel
+    sqlit
+    sqlite-devel
+    vim
+    zlib-devel
+    );;
+esac;
+
+eval "$BIN_PKG ${PKGS[@]}";
+
 git clone https://github.com/riywo/anyenv ~/.anyenv
