ffc36a39285aeecc25f605d2a3262daa65f5e11f
hdknr <gmail@hdknr.com>
2015-08-03 22:31:40 +0900

    [Visual Studio Code](VisualStudioCode)

---
+++ env/vs.bash
@@ -0,0 +1,8 @@
+# https://code.visualstudio.com/Docs/setup
+#
+# 1) http://go.microsoft.com/fwlink/?LinkID=534106
+# 2) Drag Visual Studio Code.app to the Applications folder, making it available in the Launchpad
+# 3)
+
+
+code () { VSCODE_CWD="$PWD" open -n -b "com.microsoft.VSCode" --args $* ;}
