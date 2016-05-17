94f5e97ccba1f49283505f9a2bf71b2f2dd356f1
hdknr <gmail@hdknr.com>
2016-05-17 10:13:00 +0900

    security command and python script to extrace UDID

--- env/xcode.bash
+++ env/xcode.bash
@@ -4,9 +4,6 @@ function XCODE_LISTUDID()
     GREP_OPTIONS=;
     FILE=$1
     for provisionFile in $( unzip -l "$FILE" | grep embedded.mobileprovision | awk '{$1=$2=$3=""; print $0}' | sed "s/^[ \t]*//" ); do
-        out=$(unzip -p $FILE $provisionFile | strings | awk '/ProvisionedDevices/,/\/array/'| grep string | sed 's/<[/]*string>//g' | sed 's/^/  /g');
-        number=$(echo "$out" |grep \[A-Za-z0-9] | wc -l |xargs);
-        printf "%s\n$s" $alludids $out;
+        unzip -p $FILE $provisionFile | security cms -D | python -c "import plistlib as p, sys as s; print '\n'.join(p.readPlist(s.stdin)['ProvisionedDevices']);";
     done
-
 }
