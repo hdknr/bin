0245b99cba092919126417f21466b388f1910b2d
hdknr <gmail@hdknr.com>
2016-07-22 10:50:01 +0900

    neovim + dein.vim

--- env/xcode.bash
+++ env/xcode.bash
@@ -9,3 +9,16 @@ IPA=$1
 exml=$(unzip -Z1 $IPA | GREP_OPTIONS= grep mobileprovision)
 unzip -p $IPA ${exml[0]} | security cms -D | python -c "$CODE"
 }
+
+function XCODE_INFOPLIST()
+{
+CODE="
+from plistlib import readPlist;
+from sys import stdin;
+from json import dumps
+print dumps(readPlist(stdin))
+"
+IPA=$1
+PLIST=$(unzip -Z1 $IPA | GREP_OPTIONS= grep Info.plist|head -n 1)
+unzip -p $IPA $PLIST | plutil  -convert xml1 -o - - --  | python -c "$CODE"
+}
