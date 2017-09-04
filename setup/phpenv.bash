dd80be65333d14ecbf9019c074a31b9f6c337374
Ubuntu <ubuntu@ip-172-31-30-188.ap-northeast-1.compute.internal>
2017-09-04 17:50:21 +0900

    #phpenv 7.1.8 on Ubuntu 16.04 LTS

--- setup/phpenv.bash
+++ setup/phpenv.bash
@@ -1,65 +1,18 @@
 #/bin/bash
-source `dirname $0`/conf.bash
-BIN_INIT phpenv
-
-if [ -d ~/.phpenv ]; then
-   echo "phpenv seems to to be already installed.";
-   BIN_EXIT;
-fi
-
-if [ -d ~/.anyenv/envs/phpenv ]; then
-   echo "anyenv+phpenv seems to to be already installed.";
-   BIN_EXIT;
-fi
-
-case "$BIN_OS" in 
-  "DEBIAN") 
-    PKGS=(
-    git git-core
-    curl libcurl4-gnutls-dev
-    build-essential
+PKGS=(
+    re2c
+    libxml2
     libxml2-dev
     libssl-dev
-    libjpeg-dev
-    libpng12-dev
+    bison
+    pkg-config
+    libcurl4-gnutls-dev
+    libjpeg-turbo8-dev
+    libpng-dev
     libmcrypt-dev
-    libreadline-dev
     libtidy-dev
     libxslt1-dev
     autoconf
-    );;
- "CENTOS") 
-    yum groupinstall "Development Tools";
-    PKGS=(
-    enchant-devel
-    freetype-devel
-    gettext-devel
-    gmp-devel
-    libXpm-devel
-    libc-client-devel
-    libcurl curl-devel
-    libicu libicu-devel
-    libjpeg-devel
-    libpng-devel
-    libtidy-devel
-    libxml2-devel
-    libxslt-devel
-    libxslt-devel
-    net-snmp-devel
-    t1lib-devel
-    );;
- "OSX");;
-esac;
-
-# Package Install
-$BIN_PKG "${PKGS[@]}";
-
-if [ "$(which anyevn)" == "" ]; then
-    # Install
-    curl https://raw.githubusercontent.com/CHH/phpenv/master/bin/phpenv-install.sh | bash
-
-    # php-build
-    git clone git://github.com/CHH/php-build.git ~/.phpenv/plugins/php-build
-else
-    anyenv install phpenv
-fi
+    automake
+)
+sudo apt-get update && sudo apt-get install -y  "${PKGS[@]}"
