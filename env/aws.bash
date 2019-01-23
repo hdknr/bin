aac5e4a755c6ead78babd280d6a205aad9e7bd4e
hdknr <gmail@hdknr.com>
2019-01-23 11:40:41 +0900

    AWS sercurity tools

--- env/aws.bash
+++ env/aws.bash
@@ -1,6 +1,16 @@
+# export AWS_DEFAULT_PROFILE="***AWS PROFILE NAME***"
+# export DEFAULT_SECURYT_GROUP="***DEFAULT SECURITY GROUP****""
+
+function EC2_SECGROUP_ID()
+{
+    # Print Security Group ID
+    GROUP_ID=${1:-$DEFAULT_SECURYT_GROUP}
+    aws ec2 describe-security-groups --filters Name=tag-key,Values=Name Name=tag-value,Values=$GROUP_ID | jq ".SecurityGroups[].GroupId" -r;
+}
 function EC2_SSH_SOURCE()
 {
-    # SSH の許可されたIPアドレスを一覧
+    # TCP22 address
+    GROUP_ID=${1:-$(EC2_SECGROUP_ID)}
     aws ec2 describe-security-groups --group-ids $1 | jq ".[][0].IpPermissions" | jq "map(select(.FromPort == 22))" | jq ".[].IpRanges[].CidrIp" -r
 }
 
@@ -8,17 +18,14 @@ function EC2_SSHIP_ALLOW()
 {
     # https://docs.aws.amazon.com/cli/latest/reference/ec2/authorize-security-group-ingress.html
     MYIP=`curl -s ifconfig.me`
-    aws ec2 authorize-security-group-ingress --group-id $1 --protocol tcp --port 22 --cidr $MYIP/32
+    GROUP_ID=${1:-$(EC2_SECGROUP_ID)}
+    aws ec2 authorize-security-group-ingress --group-id $GROUP_ID --protocol tcp --port 22 --cidr $MYIP/32
 }
 
 function EC2_SSHIP_REVOKE()
 {
     # https://docs.aws.amazon.com/cli/latest/reference/ec2/authorize-security-group-ingress.html
     MYIP=`curl -s ifconfig.me`
-    aws ec2 revoke-security-group-ingress  --group-id $1 --protocol tcp --port 22 --cidr $MYIP/32
-}
-function EC2_SECGROUP_ID()
-{
-    # タグ名に一致したセキュリティグループIDを表示
-    aws ec2 describe-security-groups --filters Name=tag-key,Values=Name Name=tag-value,Values=$1 | jq ".SecurityGroups[].GroupId" -r;
+    GROUP_ID=${1:-$(EC2_SECGROUP_ID)}
+    aws ec2 revoke-security-group-ingress  --group-id $GROUP_ID --protocol tcp --port 22 --cidr $MYIP/32
 }
