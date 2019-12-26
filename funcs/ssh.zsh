e5498bf9406a6d9f88bac78939dc205cc640c8a5
hdknr <gmail@hdknr.com>
2019-12-26 13:05:25 +0900

    ssh tools

---
+++ funcs/ssh.zsh
@@ -0,0 +1,68 @@
+
+function SCP()
+{
+  PARAMS="$@"; [ -n "$PARAMS" ] || PARAMS="default";
+  C=$PWD/keys;
+  while [ "$C" != / ]; do
+    if [ -f "$C/ssh.conf" ]; then
+      echo "scp -F $C/ssh.conf $PARAMS"; 
+      scp -F $C/ssh.conf $PARAMS; 
+      break;
+    fi
+    C=`dirname $C`
+  done;
+}
+
+function RSYNC()
+{
+  PARAMS="$@"; [ -n "$PARAMS" ] || PARAMS="default";
+  C=$PWD/keys;
+  while [ "$C" != / ]; do
+    if [ -f "$C/ssh.conf" ]; then
+      rsync -e "ssh -F $C/ssh.conf" $PARAMS; 
+      break;
+    fi
+    C=`dirname $C`
+  done;
+}
+
+function OPEN_SSH()
+{
+  PARAMS="$@"; [ -n "$PARAMS" ] || PARAMS="default";
+  C=$PWD/keys;
+  while [ "$C" != / ]; do
+    if [ -f "$C/ssh.conf" ]; then
+      SCRIPT="pushd .;cd $PWD;$SUDO ssh -F $C/ssh.conf $PARAMS; popd"; 
+      AS="osascript -e 'tell application \"Terminal\" to do script \"$SCRIPT\"'";
+      eval $AS; 
+      break;
+    fi
+    C=`dirname $C`
+  done;
+}
+
+function DOWNLOAD()
+{
+  # $1: HOST, $2: Rmote Path, $3: Destination
+  C=$PWD/keys;
+  while [ "$C" != / ]; do
+    if [ -f "$C/ssh.conf" ]; then
+      ssh -F $C/ssh.conf $1 "sudo tar cvfz - $2" | tar xvfz - -C $3
+    fi
+    C=`dirname $C`
+  done;
+}
+
+
+function SSH()
+{
+  PARAMS="$@"; [ -n "$PARAMS" ] || PARAMS="default";
+  C=$PWD/keys;
+  while [ "$C" != / ]; do
+    if [ -f "$C/ssh.conf" ]; then
+      ssh -F $C/ssh.conf $PARAMS;
+      break;
+    fi
+    C=`dirname $C`
+  done;
+}
