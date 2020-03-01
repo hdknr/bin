9670a2521f3040c46806c19e195369371a426670
Ubuntu <ubuntu@ip-10-0-0-236.ap-northeast-1.compute.internal>
2020-03-02 04:50:19 +0900

    env.bash

--- env.bash
+++ env.bash
@@ -1,79 +1,23 @@
+
 BASE="$HOME"
 ANYENV="$BASE/.anyenv/bin"
-#
-if [ "$(which anyenv)" == "$ANYENV/anyenv" ]; then
-  echo already there.
+
+
+if [ -d "/usr/local" ]; then
+  export PATH=$ANYENV:/usr/local/sbin:/usr/local/bin:$(getconf PATH);
 else
-  export PATH="$ANYENV:$PATH";
+  export PATH=$ANYENV:$(getconf PATH);
 fi
 
-eval "$(anyenv init -)"
-eval "$(pyenv virtualenv-init -)"
+eval "$(${ANYENV}/anyenv init -)";
 
-for D in `\ls $BASE/.anyenv/envs`; do
-    export PATH="$BASE/.anyenv/envs/$D/shims:$PATH"
-done
+anyenv envs | while read ENV; do
+    eval "$(${ENV} init -)"
+    case "$ENV" in 
+      "pyenv" ) eval "$(pyenv virtualenv-init -)";;
+    esac
+done;
 
-
-function SCP()
-{
-  PARAMS="$@"; [ -n "$PARAMS" ] || PARAMS="default";
-  C=$PWD/keys;
-  while [ "$C" != / ]; do
-    if [ -f "$C/ssh.conf" ]; then
-      echo "scp -F $C/ssh.conf $PARAMS"; 
-      scp -F $C/ssh.conf $PARAMS; 
-      break;
-    fi
-    C=`dirname $C`
-  done;
-}
-
-function RSYNC()
-{
-  PARAMS="$@"; [ -n "$PARAMS" ] || PARAMS="default";
-  C=$PWD/keys;
-  while [ "$C" != / ]; do
-    if [ -f "$C/ssh.conf" ]; then
-      rsync -e "ssh -F $C/ssh.conf" $PARAMS; 
-      break;
-    fi
-    C=`dirname $C`
-  done;
-}
-
-function DOWNLOAD()
-{
-  # $1: HOST, $2: Rmote Path, $3: Destination
-  C=$PWD/keys;
-  while [ "$C" != / ]; do
-    if [ -f "$C/ssh.conf" ]; then
-      ssh -F $C/ssh.conf $1 "sudo tar cvfz - $2" | tar xvfz - -C $3
-    fi
-    C=`dirname $C`
-  done;
-}
-
-
-function SSH()
-{
-  PARAMS="$@"; [ -n "$PARAMS" ] || PARAMS="default";
-  C=$PWD/keys;
-  while [ "$C" != / ]; do
-    if [ -f "$C/ssh.conf" ]; then
-      ssh -F $C/ssh.conf $PARAMS;
-      break;
-    fi
-    C=`dirname $C`
-  done;
-}
-
-function DJ()
-{
-    PARAMS="$@"; [ -n "$PARAMS" ] || PARAMS="shell";
-    C=$PWD;
-    while [ "$C" != / ]; do
-        [ -f "$C/manage.py" ] && { python $C/manage.py $PARAMS ; break; };
-        C=`dirname $C`
-    done;
-}
+if [ -d "${GOPATH}/bin" ]; then
+  export PATH=${GOPATH}/bin:${PATH}
+fi
