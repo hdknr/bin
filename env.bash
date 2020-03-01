d638338143bac3823d3c14458dc90d2a58222513
hdknr <gmail@hdknr.com>
2020-03-02 01:35:11 +0900

    simplify

--- env.bash
+++ env.bash
@@ -1,79 +1,18 @@
 BASE="$HOME"
 ANYENV="$BASE/.anyenv/bin"
 #
-if [ "$(which anyenv)" == "$ANYENV/anyenv" ]; then
-  echo already there.
-else
+if [ "$(which anyenv)" == "" ]; then
+  echo "anyenv initializing...";
   export PATH="$ANYENV:$PATH";
+  eval "$(anyenv init -)";
+
+  ## PATH
+  anyenv envs | while read ENV; do
+      eval "$(${ENV} init -)"
+      case "$ENV" in 
+        "pyenv" ) eval "$(pyenv virtualenv-init -)";;
+      esac
+  done;
+
 fi
 
-eval "$(anyenv init -)"
-eval "$(pyenv virtualenv-init -)"
-
-for D in `\ls $BASE/.anyenv/envs`; do
-    export PATH="$BASE/.anyenv/envs/$D/shims:$PATH"
-done
-
-
-function SCP()
-{
-  PARAMS="$@"; [ -n "$PARAMS" ] || PARAMS="default";
-  C=$PWD/keys;
-  while [ "$C" != / ]; do
-    if [ -f "$C/ssh.conf" ]; then
-      echo "scp -F $C/ssh.conf $PARAMS"; 
-      scp -F $C/ssh.conf $PARAMS; 
-      break;
-    fi
-    C=`dirname $C`
-  done;
-}
-
-function RSYNC()
-{
-  PARAMS="$@"; [ -n "$PARAMS" ] || PARAMS="default";
-  C=$PWD/keys;
-  while [ "$C" != / ]; do
-    if [ -f "$C/ssh.conf" ]; then
-      rsync -e "ssh -F $C/ssh.conf" $PARAMS; 
-      break;
-    fi
-    C=`dirname $C`
-  done;
-}
-
-function DOWNLOAD()
-{
-  # $1: HOST, $2: Rmote Path, $3: Destination
-  C=$PWD/keys;
-  while [ "$C" != / ]; do
-    if [ -f "$C/ssh.conf" ]; then
-      ssh -F $C/ssh.conf $1 "sudo tar cvfz - $2" | tar xvfz - -C $3
-    fi
-    C=`dirname $C`
-  done;
-}
-
-
-function SSH()
-{
-  PARAMS="$@"; [ -n "$PARAMS" ] || PARAMS="default";
-  C=$PWD/keys;
-  while [ "$C" != / ]; do
-    if [ -f "$C/ssh.conf" ]; then
-      ssh -F $C/ssh.conf $PARAMS;
-      break;
-    fi
-    C=`dirname $C`
-  done;
-}
-
-function DJ()
-{
-    PARAMS="$@"; [ -n "$PARAMS" ] || PARAMS="shell";
-    C=$PWD;
-    while [ "$C" != / ]; do
-        [ -f "$C/manage.py" ] && { python $C/manage.py $PARAMS ; break; };
-        C=`dirname $C`
-    done;
-}
