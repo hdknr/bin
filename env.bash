2b94a8ad5823fd4c01694df45b5a8a5af7bb56c5
hdknr <gmail@hdknr.com>
2020-03-02 02:11:01 +0900

    GOPATH

--- env.bash
+++ env.bash
@@ -1,18 +1,23 @@
+
 BASE="$HOME"
 ANYENV="$BASE/.anyenv/bin"
-#
-if [ "$(which anyenv)" == "" ]; then
-  echo "anyenv initializing...";
-  export PATH="$ANYENV:$PATH";
-  eval "$(anyenv init -)";
 
-  ## PATH
-  anyenv envs | while read ENV; do
-      eval "$(${ENV} init -)"
-      case "$ENV" in 
-        "pyenv" ) eval "$(pyenv virtualenv-init -)";;
-      esac
-  done;
 
+if [ -d "/usr/local" ]; then
+  export PATH=$ANYENV:/usr/local/sbin:/usr/local/bin:$(getconf PATH);
+else
+  export PATH=$ANYENV:$(getconf PATH);
 fi
 
+eval "$(${ANYENV}/anyenv init -)";
+
+anyenv envs | while read ENV; do
+    eval "$(${ENV} init -)"
+    case "$ENV" in 
+      "pyenv" ) eval "$(pyenv virtualenv-init -)";;
+    esac
+done;
+
+if [ -d "${GOPATH}/bin" ]; then
+  export PATH=${GOPATH}/bin:${PATH}
+fi
