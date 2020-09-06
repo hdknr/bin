601761a4eee920ea5e2f769a975d0d96df0127e7
user.name <gmail@hdknr.com>
2020-09-06 14:37:26 +0900

    vagrant resume, reload, ssh

--- funcs/vagrant.sh
+++ funcs/vagrant.sh
@@ -1,2 +1,14 @@
 alias V='vagrant'
 alias VG='vagrant global-status'
+
+VR() {
+  vagrant resume `vagrant global-status |grep virtualbox | cut -f 1 -d " " | sed -n "$1,$1p"` 
+}
+
+VL() {
+  vagrant reload `vagrant global-status |grep virtualbox | cut -f 1 -d " " | sed -n "$1,$1p"` 
+}
+
+VSSH() {
+  vagrant ssh `vagrant global-status |grep virtualbox | cut -f 1 -d " " | sed -n "$1,$1p"` 
+}
