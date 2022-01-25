fe1280c279a148f0686737e54d95a554db074c16
user.name <gmail@hdknr.com>
2022-01-25 13:27:39 +0900

    git: install sshCommand with key

---
+++ opt/git/set-sshcommand.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# .gitignore
+#   .secrets
+
+KEY=$1
+FILE=$(basename $1)
+DIR=.secrets
+#
+mkdir -p $DIR
+cp $KEY $DIR/$FINE
+#
+cat > $DIR/ssh.conf << EOS
+Host github.com
+    HostName github.com
+    Port 22
+    User git
+    IdentityFile .secrets/$FILE
+EOS
+#
+chmod 600 $DIR/ssh.conf
+git config core.sshCommand "ssh -F .secrets/ssh.conf"
